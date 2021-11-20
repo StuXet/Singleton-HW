b170f7d30f25c6968b95c53ffb229f9cef77e007
StuXet <36419721+StuXet@users.noreply.github.com>
2021-11-20 20:41:02 +0200

    Upload HW

---
+++ CarEngine/CarEngine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CarEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarEngine engine = CarEngine.Instance;
+            Console.WriteLine(engine.model + "\n" + engine.maxSpeed + "\n" + engine.Year);
+            engine.DoSomething();
+        }
+    }
+
+    public class CarEngine
+    {
+        private static CarEngine instance = null;
+        private CarEngine() { }
+
+        public static CarEngine Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CarEngine();
+                }
+                return instance; 
+            }
+        }
+
+        public int maxSpeed = 4600;
+        public int Year = 2002;
+        public string model = "Volvo D5 engine";
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Vroom Vroom");
+        }
+    }
+}
